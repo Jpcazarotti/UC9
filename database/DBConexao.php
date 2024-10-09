bd20a79ba4e287c7e24d4b8094c16ef54d8b3a87
Joao Pedro Cazarotti Batista <joaopedrocazarottijp@gmail.com>
2024-10-09 13:53:15 -0300

    Primeiros arquivos criados

---
+++ database/DBConexao.php
@@ -0,0 +1,38 @@
+<?php
+
+class DBConexao
+{
+
+    private $host = "localhost";
+    private $dbname = "biblioteca";
+    private $username = "root";
+    private $password = "senac2023";
+
+    private $conx;
+    private static $instance = null;
+
+    public function __construct()
+    {
+        try{
+            // Inicializar a conexão
+            $this->conx = new PDO("mysql:host=$this->host;dbname=$this->dbname;charset=utf-8", $this->username, $this->password);
+            $this->conx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);            
+        }
+        catch(PDOException $e){
+            // Capturar o erro da conexão
+            echo "Erro na conexão com o banco de dados: " . $e->getMessage();
+        }
+    }   
+    
+    /**
+     * Método estatico para a instancia única da conexão
+     */
+
+    public static function getConexao()
+    {
+        if(!self::$instance){
+            self::$instance = new DBConexao();
+        }
+        return self::$instance->conx;
+    }
+}
